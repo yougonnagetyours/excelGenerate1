04806485648b697d21d7a8074e7cd76461a06ade
Assembly13 <assemblyinstruction13@gmail.com>
2020-10-12 13:09:02 +0000

    Consider pushing elements of array - e.g. map function etc.

--- src/App.js
+++ src/App.js
@@ -7,57 +7,17 @@ const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
 const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
-const dataSet1 = [
-    {
-        name: "Johson",
-        amount: 30000,
-        sex: 'M',
-        is_married: true
-    },
-    {
-        name: "Monika",
-        amount: 355000,
-        sex: 'F',
-        is_married: false
-    },
-    {
-        name: "John",
-        amount: 250000,
-        sex: 'M',
-        is_married: false
-    },
-    {
-        name: "Josef",
-        amount: 450500,
-        sex: 'M',
-        is_married: true
-    }
-];
-
-var dataSet2 = [
-    {
-        name: "Johnson",
-        total: 25,
-        remainig: 16
-    },
-    {
-        name: "Josef",
-        total: 25,
-        remainig: 7
-    }
-];
 
 class Download extends React.Component {
   state= {
-    internalWidth: '',
+    internalWidth: null,
     internalDepth: '',
     internalHeight: '',
     dataSet1: [
       {
-        name: 'josh',
-        amount: 30000,
-        sex: 'M',
-        is_married: true
+        function: '',
+        dim1: '',
+        dim2: '',
       }
     ]
   }
@@ -80,12 +40,29 @@ class Download extends React.Component {
     })
     console.log('click')
   }
+
+  handleAcceptButton = () =>{
+    this.setState({
+      dataSet1: [
+      {
+        function: 'bottom',
+        dim1: this.state.internalWidth+7,
+        dim2: this.state.internalDepth,
+      },
+      {
+        function: 'bottom',
+        dim1: this.state.internalWidth,
+        dim2: this.state.internalDepth,
+      }
+    ]
+    })
+  }
     render() {
         return (
           <div className="wrapper"> 
             <div className="form-group">
                 <label htmlFor="iw">Internal width</label>
-                <input type="text" id="iw" className="form-control" value={this.state.internalWidth} onChange={this.handleIWChange}/>
+                <input type="number" id="iw" className="form-control" value={this.state.internalWidth} onChange={this.handleIWChange}/>
             </div>
             <div className="form-group">
                 <label htmlFor="id">Internal depth</label>
@@ -95,19 +72,12 @@ class Download extends React.Component {
                 <label htmlFor="ih">Internal height</label>
                 <input type="text" id="ih" className="form-control" value={this.state.internalHeight} onChange={this.handleIHChange}/>
             </div>
-            <button className="accept">Accept</button>
+            <button className="accept" onClick={this.handleAcceptButton}>Accept</button>
             <ExcelFile element={<button>Download Data</button>}>
                 <ExcelSheet data={this.state.dataSet1} name="Employees">
-                    <ExcelColumn label="Name" value="name"/>
-                    <ExcelColumn label="Wallet Money" value="amount"/>
-                    <ExcelColumn label="Gender" value="sex"/>
-                    <ExcelColumn label="Marital Status"
-                                 value={(col) => col.is_married ? "Married" : "Single"}/>
-                </ExcelSheet>
-                <ExcelSheet data={dataSet2} name="Leaves">
-                    <ExcelColumn label="Name" value="name"/>
-                    <ExcelColumn label="Total Leaves" value="total"/>
-                    <ExcelColumn label="Remaining Leaves" value="remaining"/>
+                    <ExcelColumn label="Function" value="function"/>
+                    <ExcelColumn label="Dim 1 MM" value="dim1"/>
+                    <ExcelColumn label="Dim 2 MM" value="dim2"/>
                 </ExcelSheet>
             </ExcelFile>
           </div>
